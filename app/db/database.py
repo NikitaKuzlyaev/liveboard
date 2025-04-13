5e86633299550e8460ba403503f8da3d2fc8c88a
NikitaKuzlyaev <kuzliaev.nikita@yandex.ru>
2025-04-13 10:40:39 +0700

    add local project to git

---
+++ app/db/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import as_declarative
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = os.getenv("DEBUG") == "True"
+
+if DEBUG:
+    DATABASE_URL = os.getenv("DATABASE_DEBUG_URL")
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Создаем асинхронный движок для базы данных
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+# Асинхронная сессия
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+# Генератор сессий для работы с базой данных
+async def get_db():
+    async with async_session() as session:
+        yield session
